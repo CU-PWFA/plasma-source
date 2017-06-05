b0463fd71647e03defbf528fdaf6f0d22e9679ab
Chris <chdo6482@colorado.edu>
2017-06-05 16:18:15 -0600

    Updated cylindrical beam code and added more plots

--- cedoss/Test_RealisticCylLens.py
+++ cedoss/Test_RealisticCylLens.py
@@ -3,23 +3,41 @@
 
 @author: chris
 """
+import sys
+
 
 import GaussianBeam
 import numpy as np
 import matplotlib.pyplot as plt
-import ADK_Combined as adk
+import ADK_Combined as adkD
 import DensityDistributions as dendis
-import sys
+
+sys.path.insert(0, "../python")
+
+from ionization import ionization
+from ionization import adk
+
+#Flags for plotting different things
+cut_plots_H = 0
+contour_plots = 0
+den_plot = 0
+cut_plots_den = 1
 
 c=2.998e8
-P=12e9
+P=15e9
 cm_m=100
 wavelength = 785.3e-9
 w0 = 5e-3
 
 I0 = 2*P/(np.pi*np.power(w0,2))*np.power(1/cm_m,2)
 delt_t = 100e-15
-chi = adk.Get_chi_H()
+chi = adkD.Get_chi_H()
+
+L_z = 500e-6
+L_r = 500e-6
+nozzle_loc = 200e-6
+nozzle_den = 1e18
+
 choice=0
 if choice==0:
     q_y = GaussianBeam.Prop_Init_q(wavelength, w0, -3.5, 1)
@@ -59,7 +77,7 @@ plt.plot(xrange_tot,wtotz)
 plt.show()
 
 waist=wtoty.index(min(wtoty))
-zoom=100
+zoom=50
 wy=wtoty[(waist-zoom):(waist+zoom)]
 wz=wtotz[(waist-zoom):(waist+zoom)]
 
@@ -80,49 +98,61 @@ if choice==1:
 
 #Create a 3D array for the intensity of this guassian beam as a function of
 # both w(z) and r
-rrange=np.arange(-200e-6,200e-6,.5e-5)
-I=np.empty([len(xrange),len(rrange),len(rrange)])
+yrange=np.arange(-50e-6,50e-6,1e-6)
+zrange=np.arange(-200e-6,200e-6,1e-6)
+
+I=np.empty([len(xrange),len(yrange),len(zrange)])
 i_counter=np.arange(len(xrange))
-j_counter=np.arange(len(rrange))
-k_counter=np.arange(len(rrange))
+j_counter=np.arange(len(yrange))
+k_counter=np.arange(len(zrange))
 for i in i_counter:
     for j in j_counter:
         for k in k_counter:
-            I[i][j][k]=GaussianBeam.GaussianBeamIntensity_SpotArray_2D(I0,wy[i],wz[i],w0,rrange[j],rrange[k])
+            I[i][j][k]=GaussianBeam.GaussianBeamIntensity_SpotArray_2D(I0,wy[i],wz[i],w0,yrange[j],zrange[k])
 
-H=np.empty([len(xrange),len(rrange),len(rrange)])
-for i in i_counter:
-    for j in j_counter:
-        H[i][j] = adk.TotalProb(adk.Elefield(I[i][j]),delt_t,chi,1)
+H=np.empty([len(xrange),len(yrange),len(zrange)])\
+
+square_wave=0
+if square_wave == 1:
+    for i in i_counter:
+        for j in j_counter:
+            H[i][j] = adkD.TotalProb(adkD.Elefield(I[i][j]),delt_t,chi,1)
+else:
+    for i in i_counter:
+        for j in j_counter:
+            E = ionization.field_from_intensity(I[i][j]/1e14)
+            H[i][j] = adk.gaussian_frac(chi,E,delt_t/1e-15,1)
 
 waist_loc = xrange[round(len(xrange)/2)]
 xrange=[i-waist_loc for i in xrange]
 
-cut_plots = 0
-if cut_plots==1:
-    H_cut=H[:,round(len(rrange)*1/4):round(len(rrange)*3/4),:]
-    rrange_cut=rrange[round(len(rrange)*1/4):round(len(rrange)*3/4)]
-    H_center    = H_cut[round(len(xrange)/2),:,round(len(rrange)/2)]
-    H_center_dz = H_cut[round(len(xrange)/2),:,round(len(rrange)/2)+50]
-    H_center_dz2 = H_cut[round(len(xrange)/2),:,round(len(rrange)/2)+100]
-    rrange_zoomed=[i*1e6 for i in rrange_cut]
+if cut_plots_H==1:
+    H_cut=H[:,round(len(yrange)*1/4):round(len(yrange)*3/4),:]
+    yrange_cut=yrange[round(len(yrange)*1/4):round(len(yrange)*3/4)]
+    H_center    = H_cut[round(len(xrange)/2),:,round(len(zrange)/2)]
+    H_center_dz = H_cut[round(len(xrange)/2),:,round(len(zrange)/2)+20]
+    H_center_dz2 = H_cut[round(len(xrange)/2),:,round(len(zrange)/2)+40]
+    H_center_dz3 = H_cut[round(len(xrange)/2),:,round(len(zrange)/2)+60]
+    rrange_zoomed=[i*1e6 for i in yrange_cut]
     plt.plot(rrange_zoomed,H_center)
     plt.title("Ion. Frac. through waist")
     plt.ylabel("ni/n0")
     plt.xlabel("Radius from axis (microns)")
-    plt.plot(rrange_zoomed,H_center_dz) #Orange is 50microns in z direction
-    plt.plot(rrange_zoomed,H_center_dz2) #Green is 100microns in z direction
+    plt.plot(rrange_zoomed,H_center_dz) #Orange is 20 microns in z direction
+    plt.plot(rrange_zoomed,H_center_dz2) #Green is 40 microns in z direction
+    plt.plot(rrange_zoomed,H_center_dz3) #Red is 60 microns
     plt.show()
 
-contour_plots = 1
 if contour_plots==1:
-    rrange_z=[i*1000 for i in rrange]
+    xrange_z=[i*1000 for i in xrange]
+    yrange_z=[i*1000 for i in yrange]
+    zrange_z=[i*1000 for i in zrange]
     #Plot intensity at x=waist
     I_atwaist=np.transpose(I[round(len(xrange)/2)])
     plt.imshow(I_atwaist, interpolation="none", origin="lower",
-                 extent=[rrange_z[0],rrange_z[-1],rrange_z[0],rrange_z[-1]], aspect='1')
+                 extent=[yrange_z[0],yrange_z[-1],zrange_z[0],zrange_z[-1]], aspect='.5')
     CB=plt.colorbar()
-    CB.set_label('I (I0 = 3e10')
+    CB.set_label('I (I0 = 3.8e10')
     plt.ylabel('z (mm)')
     plt.xlabel('y (mm)')
     plt.title('Intensity; x=waist')
@@ -130,7 +160,7 @@ if contour_plots==1:
 
     H_atwaist=np.transpose(H[round(len(xrange)/2)])
     plt.imshow(H_atwaist, interpolation="none", origin="lower",
-                 extent=[rrange_z[0],rrange_z[-1],rrange_z[0],rrange_z[-1]], aspect='1')
+                 extent=[yrange_z[0],yrange_z[-1],zrange_z[0],zrange_z[-1]], aspect='.5')
     CB=plt.colorbar()
     CB.set_label('ni/n0')
     plt.ylabel('z (mm)')
@@ -139,83 +169,142 @@ if contour_plots==1:
     plt.show()
 
     #Plot intensity at y=0
-    I_yzero=I[:,round(len(rrange)/2),:]
+    I_yzero=I[:,round(len(yrange)/2),:]
     plt.imshow(I_yzero, interpolation="none", origin="lower",
-               extent=[rrange_z[0],rrange_z[-1],xrange[0],xrange[-1]],aspect='10')
+               extent=[zrange_z[0],zrange_z[-1],xrange_z[0],xrange_z[-1]],aspect='.02')
     CB=plt.colorbar()
-    CB.set_label('I (I0 = 3e10')
-    plt.ylabel('x (m)')
+    CB.set_label('I (I0 = 3.8e10')
+    plt.ylabel('x (mm)')
     plt.xlabel('z (mm)')
     plt.title('Intensity; y=0')
     plt.show()
     
-    H_yzero=H[:,round(len(rrange)/2),:]
+    H_yzero=H[:,round(len(yrange)/2),:]
     plt.imshow(H_yzero, interpolation="none", origin="lower",
-               extent=[rrange_z[0],rrange_z[-1],xrange[0],xrange[-1]], aspect='10')
+               extent=[zrange_z[0],zrange_z[-1],xrange_z[0],xrange_z[-1]], aspect='.02')
     CB=plt.colorbar()
     CB.set_label('ni/n0')
-    plt.ylabel('x (m)')
+    plt.ylabel('x (mm)')
     plt.xlabel('z (mm)')
     plt.title('ADK Ion.; y=0')
     plt.show()
 
     #Plot intensity at z=0
-    I_zzero=I[:,:,round(len(rrange)/2)]
+    I_zzero=I[:,:,round(len(zrange)/2)]
     plt.imshow(I_zzero, interpolation="none", origin="lower",
-               extent=[rrange_z[0],rrange_z[-1],xrange[0],xrange[-1]],aspect='10')
+               extent=[yrange_z[0],yrange_z[-1],xrange_z[0],xrange_z[-1]],aspect='.01')
     CB=plt.colorbar()
-    CB.set_label('I (I0 = 3e10')
-    plt.ylabel('x (m)')
+    CB.set_label('I (I0 = 3.8e10')
+    plt.ylabel('x (mm)')
     plt.xlabel('y (mm)')
     plt.title('Intensity; z=0')
     plt.show()
 
-    H_zzero=H[:,:,round(len(rrange)/2)]
+    H_zzero=H[:,:,round(len(zrange)/2)]
     plt.imshow(H_zzero, interpolation="none", origin="lower",
-               extent=[rrange_z[0],rrange_z[-1],xrange[0],xrange[-1]], aspect='10')
+               extent=[yrange_z[0],yrange_z[-1],xrange_z[0],xrange_z[-1]], aspect='.01')
     CB=plt.colorbar()
     CB.set_label('ni/n0')
-    plt.ylabel('x (m)')
+    plt.ylabel('x (mm)')
     plt.xlabel('y (mm)')
     plt.title('ADK Ion.; z=0')
     plt.show()
     
-den_plot = 1
-if den_plot == 1:
-    den=np.empty([len(xrange),len(rrange),len(rrange)])
+if den_plot + cut_plots_den > 0:
+    den=np.empty([len(xrange),len(yrange),len(zrange)])
     for x in i_counter:
         for y in j_counter:
             for z in k_counter:
-                den[x][y][z]=H[x][y][z]*dendis.SimpleGasJet(1e18,500e-6,500e-6,200e-6,xrange[x],rrange[y],rrange[z])
-
-    rrange_z=[i*1000 for i in rrange]
+                den[x][y][z]=H[x][y][z]*dendis.SimpleGasJet(nozzle_den,L_z,L_r,nozzle_loc,xrange[x],yrange[y],zrange[z])
+if den_plot == 1:
+    
+    xrange_z=[i*1000 for i in xrange]
+    yrange_z=[i*1000 for i in yrange]
+    zrange_z=[i*1000 for i in zrange]
 
     den_atwaist=np.transpose(den[round(len(xrange)/2)])
     plt.imshow(den_atwaist, interpolation="none", origin="lower",
-                 extent=[rrange_z[0],rrange_z[-1],rrange_z[0],rrange_z[-1]], aspect='1')
+                 extent=[yrange_z[0],yrange_z[-1],zrange_z[0],zrange_z[-1]], aspect='.5')
     CB=plt.colorbar()
-    CB.set_label('ni')
+    CB.set_label('n')
     plt.ylabel('z (mm)')
     plt.xlabel('y (mm)')
     plt.title('Plasma Density; x=waist')
     plt.show()
 
-    den_yzero=den[:,round(len(rrange)/2),:]
+    den_yzero=den[:,round(len(yrange)/2),:]
     plt.imshow(den_yzero, interpolation="none", origin="lower",
-               extent=[rrange_z[0],rrange_z[-1],xrange[0],xrange[-1]], aspect='10')
+               extent=[zrange_z[0],zrange_z[-1],xrange_z[0],xrange_z[-1]], aspect='.02')
     CB=plt.colorbar()
-    CB.set_label('ni')
-    plt.ylabel('x (m)')
+    CB.set_label('n')
+    plt.ylabel('x (mm)')
     plt.xlabel('z (mm)')
     plt.title('Plasma Density; y=0')
     plt.show()
 
-    den_zzero=den[:,:,round(len(rrange)/2)]
+    den_zzero=den[:,:,round(len(zrange)/2)]
     plt.imshow(den_zzero, interpolation="none", origin="lower",
-               extent=[rrange_z[0],rrange_z[-1],xrange[0],xrange[-1]], aspect='10')
+               extent=[yrange_z[0],yrange_z[-1],xrange_z[0],xrange_z[-1]], aspect='.01')
     CB=plt.colorbar()
-    CB.set_label('ni')
-    plt.ylabel('x (m)')
+    CB.set_label('n')
+    plt.ylabel('x (mm)')
     plt.xlabel('y (mm)')
-    plt.title('ADK Ion.; z=0')
+    plt.title('Plasma Density; z=0')
     plt.show()
+    
+if cut_plots_den == 1:
+    xrange_z=[i*1000 for i in xrange]
+    yrange_z=[i*1000 for i in yrange]
+    zrange_z=[i*1000 for i in zrange]
+    
+    n_vs_z   = den[round(len(xrange_z)/2),round(len(yrange_z)/2),:]
+    n_vs_y   = den[round(len(xrange_z)/2),:,round(len(zrange_z)/2)]
+    n_vs_y1  = den[round(len(xrange_z)/2),:,round(len(zrange_z)/2)+20]
+    n_vs_y2  = den[round(len(xrange_z)/2),:,round(len(zrange_z)/2)+40]
+    n_vs_y3  = den[round(len(xrange_z)/2),:,round(len(zrange_z)/2)+60]
+    n_vs_y2d = den[round(len(xrange_z)/2),:,round(len(zrange_z)/2)-40]
+    
+    plt.plot(zrange_z,n_vs_z)
+    plt.title("Plasma density along gas jet")
+    plt.ylabel("n")
+    plt.xlabel("Radius from laser axis (mm)")
+    plt.show()
+    
+    plt.plot(yrange_z,n_vs_y)
+    plt.title("Plasma density along beam axis")
+    plt.ylabel("n")
+    plt.xlabel("Radius from beam axis (mm)")
+    plt.show()
+    
+    plt.plot(yrange_z,n_vs_y)
+    plt.title("Plasma density along beam axis")
+    plt.ylabel("n")
+    plt.xlabel("Radius from beam axis (mm)")
+    plt.plot(yrange_z,n_vs_y1)
+    plt.plot(yrange_z,n_vs_y2)
+    plt.plot(yrange_z,n_vs_y3)
+    plt.show()
+    
+    plt.plot(yrange_z,n_vs_y2)
+    plt.title("Plasma density along beam axis")
+    plt.ylabel("n")
+    plt.xlabel("Radius from beam axis (mm)")
+    plt.plot(yrange_z,n_vs_y2d)
+    plt.show()
+    
+    compare=0
+    if compare ==1:
+        den2=np.empty([len(xrange),len(yrange),len(zrange)])
+        for x in i_counter:
+            for y in j_counter:
+                for z in k_counter:
+                    den2[x][y][z]=H[x][y][z]*dendis.SimpleGasJet(nozzle_den,L_z,L_r,2*nozzle_loc,xrange[x],yrange[y],zrange[z])
+                    n_vs_z2   = den2[round(len(xrange_z)/2),round(len(yrange_z)/2),:]
+                    plt.plot(zrange_z,n_vs_z)
+                    plt.plot(zrange_z,n_vs_z2)
+                    plt.title("Plasma density along gas jet")
+                    plt.ylabel("n")
+                    plt.xlabel("Radius from laser axis (mm)")
+                    plt.show()
+                
+                
