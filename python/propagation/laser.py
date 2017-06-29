6281dd31b8b790885ebecdd41a05305523d88626
rariniello <roar3446@colorado.edu>
2017-06-29 17:36:25 -0600

    Fixed a small bug if arrays were reshaped before being passed in.

--- python/propagation/laser.py
+++ python/propagation/laser.py
@@ -508,10 +508,10 @@ def fresnel_axis(E, r, z, lam, n=1):
     """
     Nz = np.size(z)
     k = 2*np.pi*n/lam
-    # Integral prefactor
-    pre = k*np.exp(1j*k*z) / (1j*z)
     # Reshape to create the Nz X Nr array for integration
     z = np.reshape(z, (Nz, 1))
+    # Integral prefactor
+    pre = k*np.exp(1j*k*z) / (1j*z)
     # Calculate the argument of the integral
     arg = E * np.exp(1j*k*r**2/(2*z)) * r  
     e = pre * integrate.simps(arg, r, axis=1)
