4f6cb392a5bccff8b19180009f278b76072a8573
rariniello <roar3446@colorado.edu>
2017-07-24 17:46:56 -0600

    Renamed a repeated variable to avoid confusion.

--- python/lens/bessel.py
+++ python/lens/bessel.py
@@ -71,7 +71,7 @@ def kr_from_kz(kz, lam, S=None):
         return kr
 
 
-def uniform_bessel(params, E, z, n=0):
+def uniform_bessel(params, Ez, z, n=0):
     """ Calculate the required electric field to create the passed intensity.
 
     Calculates the electric field necessary to create the passed intensity
@@ -94,7 +94,7 @@ def uniform_bessel(params, E, z, n=0):
                 Wavelength of the electromagnetic wave in vacuum.
             rmax : double
                 Maximum radius to return the electric field at.
-    E : array-like
+    Ez : array-like
         Desired on axis field in GV/m.
     z : array-like
         Array of z coordinates along the optical axis. Must be evenly spcaed
@@ -112,7 +112,7 @@ def uniform_bessel(params, E, z, n=0):
     #TODO figure out if the normalization is just not enough steps or an error
     lam = params['lam']
     k = 2*np.pi/lam
-    kz, S = spectrum_from_axis(E, z)
+    kz, S = spectrum_from_axis(Ez, z)
     # Add the linear phase based of the width of the Bessel beam
     kr0 = 2.4048 / params['R']
     kz0 = np.sqrt(k**2 - kr0**2)
