af3ced876e1c625c3d76985543d3de10deb6e58a
rariniello <roar3446@colorado.edu>
2017-09-08 14:43:56 -0600

    Completed a fully functioning cython version of fourier_prop2 along with an accompanying setup file.

--- python/propagation/laserv2.pyx
+++ python/propagation/laserv2.pyx
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #cython: boundscheck=False, wraparound=False, nonecheck=False, overflowcheck=False
-#cython: linetrace=False
+#cython: linetrace=True, binding=True
 """
 Created on Fri Sep  1 14:07:46 2017
 
@@ -10,35 +10,17 @@ Created on Fri Sep  1 14:07:46 2017
 
 import os
 import numpy as np
+cimport numpy as np
 from numpy.fft import fftfreq
 from cython.parallel import prange
 from libc.stdio cimport FILE, fwrite, fopen, fclose
+import pyfftw
 
 
 # Load necessary C functions
 cdef extern from "complex.h" nogil:
     double complex cexp(double complex)
     double complex csqrt(double complex)
-
-
-cdef extern from "fftw3.h":
-    # Load constants from fftw
-    cdef int FFTW_FORWARD
-    cdef int FFTW_BACKWARD
-    cdef unsigned FFTW_MEASURE
-    cdef unsigned FFTW_ESTIMATE
-    # Memory allocation
-    void *fftw_malloc(size_t)
-    void fftw_free(void *)
-    # Define cython structure that point to the fftw_plan structure
-    ctypedef struct _fftw_plan:
-        pass
-    ctypedef _fftw_plan *fftw_plan
-    # Load fftw functions: plan, execute, destroy
-    fftw_plan fftw_plan_dft_2d(int, int, double complex*, double complex*, int,
-                               unsigned)
-    void fftw_execute(fftw_plan) nogil
-    void fftw_destroy_plan(fftw_plan)
 
 
 cdef double complex I = 1j
@@ -91,7 +73,7 @@ def fourier_prop2(double complex[:, :] E, double[:] x, double[:] y,
     Returns
     -------
     e : array_like
-        The electric field at position (z, x, y).
+        The electric field at position (Z, x, y).
     """
     # Need these to calculate frequencies later on
     cdef int i, j, k
@@ -103,27 +85,14 @@ def fourier_prop2(double complex[:, :] E, double[:] x, double[:] y,
     cdef double Y = y[Ny-1] - y[0]
     cdef double dx = X / (Nx-1)
     cdef double dy = Y / (Ny-1)
-    filename_py = bytes(path + 'electricField.bin', 'ascii')
-    if os.path.isfile(filename_py):
-        os.remove(filename_py) # Otherwise we'll append to it
-    cdef char *filename = filename_py
     # Initialize fftw for the FFT
-    cdef double complex[:, :] e = <double complex[:Nx, :Ny]> fftw_malloc(sizeof(double complex) * N)
-    cdef fftw_plan pfft = fftw_plan_dft_2d(Nx, Ny, &(e[0, 0]), &(e[0, 0]), FFTW_FORWARD,
-                                           FFTW_ESTIMATE)
-    cdef fftw_plan pifft = fftw_plan_dft_2d(Nx, Ny, &(e[0, 0]), &(e[0, 0]), FFTW_BACKWARD,
-                                           FFTW_ESTIMATE)
+    e = pyfftw.empty_aligned((Nx, Ny), dtype='complex128')
+    cdef double complex[:, :] e_view = e
+    pfft = pyfftw.builders.fft2(e, overwrite_input=True, avoid_copy=True, threads=4)
+    pifft = pyfftw.builders.ifft2(e, overwrite_input=True, avoid_copy=True, threads=4)
     # Fourier transform the electric field on the boundary
-    with nogil:
-        for i in prange(Nx):
-            for j in range(Ny):
-                e[i, j] = E[i, j]
-    fftw_execute(pfft)
     cdef double complex[:, :] eb = np.zeros((Nx, Ny), dtype='complex128')
-    with nogil:
-        for i in prange(Nx):
-            for j in range(Ny):
-                eb[i, j] = e[i, j]
+    eb = pfft(E)
     # Pre-calculate the spatial frequencies
     cdef double complex[:, :] fz = np.zeros((Nx, Ny), dtype='complex128')
     cdef double[:] fx2 = fftfreq(Nx, dx)**2
@@ -134,21 +103,11 @@ def fourier_prop2(double complex[:, :] E, double[:] x, double[:] y,
         for i in prange(Nx):
             for j in range(Ny):
                 fz[i, j] = pre*csqrt(fmax2 - fx2[i] - fy2[j])
-        for i in range(Nz):
+    for i in range(Nz):
+        with nogil:
             for j in prange(Nx):
                 for k in range(Ny):
-                    e[j, k] = eb[j, k] * cexp(fz[j, k]*z[i])
-            fftw_execute(pifft)
-            write_data(&(e[0, 0]), filename, N)
-    fftw_destroy_plan(pfft)
-    fftw_destroy_plan(pifft)
-    Eret = np.zeros((Nx, Ny), dtype='complex128')
-    ebret = np.zeros((Nx, Ny), dtype='complex128')
-    eRet = np.zeros((Nx, Ny), dtype='complex128')
-    for i in range(Nx):
-        for j in range(Ny):
-            Eret[i, j] = E[i, j]
-            ebret[i, j] = eb[i, j]
-            eRet[i, j] = e[i, j]
-    fftw_free(&(e[0, 0]))
-    return Eret, ebret, eRet
+                    e_view[j, k] = eb[j, k] * cexp(fz[j, k]*z[i])
+        e = pifft()
+        np.save('electricField'+str(i)+'.npy', e)
+    return e
--- python/propagation/setup.py
+++ python/propagation/setup.py
@@ -9,6 +9,9 @@ Run from the python folder with the command:
     'python propagation/setup.py build_ext --inplace'
 """
 
+import numpy
+import venv
+import os
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
@@ -18,11 +21,14 @@ ext_modules=[
     Extension('propagation.laserv2',
               sources=['propagation/laserv2.pyx'],
               libraries=["m", "fftw3"],
+              include_dirs=[numpy.get_include(), 
+                            os.path.join(venv.sys.base_prefix, 'include')],
               extra_compile_args = ['-march=native', '-fopenmp', '-O3',
                                     #'-DCYTHON_TRACE_NOGIL=1',
-                                    '-I/home/robert/anaconda3/envs/CU-PWFA/include', 
+                                    #'-I/home/robert/anaconda3/envs/CU-PWFA/include', 
                                     '-lfftw3'],
-              extra_link_args=['-fopenmp']
+              extra_link_args=['-fopenmp'],
+              define_macros=[('CYTHON_TRACE', '1')]
     )
 ]
 
