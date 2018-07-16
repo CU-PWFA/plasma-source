ddae85e501ba0523cc1bf579295433d7aadc684f
kehunt <kehu2128@colorado.edu>
2018-07-16 17:25:10 -0600

    Update

--- khuntstone/Constants/SI.py
+++ khuntstone/Constants/SI.py
@@ -1,5 +1,7 @@
 # Defining natural constants for use later 
 
-elemCharge = 1.60217662e-19 # Coulombs
+elemCharge    = 1.60217662e-19 # Coulombs
 permFreeSpace = 8.85418782e-12 # Farad /m 
 lightSpeed    = 299792458.0 # m / s
+elecRestMass      = 0.511e6; # eV 
+elecMass      = 9.10938356e-31;
