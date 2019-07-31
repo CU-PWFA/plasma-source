052b4f6c1dfe36bc2bdea8885f31c6c6a8c2cb1f
rariniello <roar3446@colorado.edu>
2019-07-31 16:49:17 -0600

    Added energy loss from field ionization to the simulation code.

--- python/beam/calc/plasma.pyx
+++ python/beam/calc/plasma.pyx
@@ -94,6 +94,79 @@ def plasma_refraction(double complex[:, :, :] E, double[:] x, double[:] y,
                   ne[k, l] = 0.0                           
     return E
 
+
+def plasma_refraction_energy(double complex[:, :, :] E, double[:] x, double[:] y,
+                      double[:] z, double[:] t, double lam, double n0, 
+                      double z0, fft, ifft, saveE, saven, atom, loadn, loadne):
+    """ Propagate a laser pulse through a plasma accounting for refraction.
+
+    Propogates a laser pulse through a region of partially ionized gas. This
+    function accounts for refraction from the plasma. It determines the plasma
+    density by calculating the ionization that has resulted from each temporal
+    piece of the pulse. The results are stored in a file, only the central x-z
+    plane is recorded. Note that z=0 
+    """
+    cdef int i, j, k, l
+    # TODO abstract this into its own function
+    cdef int Nx = len(x)
+    cdef int Ny = len(y)
+    cdef int Nz = len(z)
+    cdef int Nt = len(t)
+    cdef double dx = x[1] - x[0]
+    cdef double dy = y[1] - y[0]
+    cdef double dt = t[1] - t[0]
+    # Get the ionization information from atom
+    cdef double EI = atom['EI']
+    cdef int Z = atom['Z']
+    cdef int ll = atom['l']
+    cdef int m = atom['m']
+    # Plasma density and index of refraction arrays
+    # n is total number density, ng + ne
+    cdef double[:, :] n = np.zeros((Nx, Ny), dtype='double')
+    cdef double[:, :] ne = np.zeros((Nx, Ny), dtype='double')
+    cdef double ngas = atom['alpha'] * 5.0e-8
+    cdef double nplasma = plasma_index(1.0, lam) - 1.0
+    cdef double nh = 1.0 + ngas*n0
+    cdef double dn = nplasma - ngas
+    # Pre-calculate the spatial frequencies
+    cdef double[:] fx = fftfreq(Nx, dx)
+    cdef double[:] fy = fftfreq(Ny, dy)
+    cdef double complex[:, :] ikz = laser.ikz_RS(fx, fy, lam, nh)
+    cdef double complex arg
+    cdef double rate
+    cdef double Eavg
+    cdef double dz
+    cdef double ne_new
+    cdef double dE
+    cdef double complex[:, :] e = np.zeros((Nx, Ny), dtype='complex128')
+    for i in range(1, Nz):
+        n = loadn(i-1)
+        ne = loadne(i-1)
+        dz = z[i] - z[i-1]
+        arg = 1j*2*np.pi*dz*dn / lam
+        for j in range(Nt):
+            # Propagate the beam through
+            e = laser.fourier_step(E[j, :, :], ikz, dz, fft, ifft)
+            with nogil:
+                for k in prange(Nx):
+                    for l in range(Ny):
+                        e[k, l] *= cexp(arg*ne[k, l])
+                        # Ionize the gas
+                        Eavg = 0.5*(cabs(E[j, k, l]) + cabs(e[k, l]))
+                        rate = adk_rate_linear(EI, Eavg, Z, ll, m)
+                        ne_new = n[k, l]-(n[k, l]-ne[k, l])*exp(-rate*dt)
+                        # Remove energy from the laser
+                        dE = energy_loss(ne[k, l], ne_new, EI, dz, dt, e[k, l])
+                        ne[k, l] = ne_new
+                        e[k, l] *= dE
+            E[j, :, :] = e
+        saveE(E, z[i]+z0)
+        saven(ne, i-1)
+        for k in range(Nx):
+            for l in range(Ny):
+                  ne[k, l] = 0.0                           
+    return E
+
 # TODO, move this to a helper function file
 cpdef double plasma_index(double n, double lam):
     """ Calculates the index of refraction of a plasma.
@@ -107,7 +180,7 @@ cpdef double plasma_index(double n, double lam):
     """
     return 1.0 - n * lam*lam * 4.47869e-5
 
-cpdef double energy_loss(double n_i, double n_f, double EI, double dz, double dt):
+cdef double energy_loss(double n_i, double n_f, double EI, double dz, double dt, double complex E0) nogil:
     """ Calculate the decrease in field strength from ionization energy loss.
     
     Parameters
@@ -121,9 +194,14 @@ cpdef double energy_loss(double n_i, double n_f, double EI, double dz, double dt
     dz : double
         Length of the cell in um.
     dt : double
-        Length of the time slice in seconds.
+        Length of the time slice in fs.
+    E0 : double complex
+        Current electric field of the pulse.
     """
-    cdef double energy = (n_f - n_i)*EI*dz*1.602e-6
-    cdef double dE = sqrt(energy/dt/1.32721e11)
-    return dE
+    cdef double a
+    a = 1.207e-2*EI*(n_f-n_i)*dz / (dt*cabs(E0)**2)
+    if a < 1:
+        return sqrt(1-a)
+    else:
+        return 0.0
     
