74202dbc433aa58d84ecc54822c3a27b5ede9e01
Keenan Hunt-Stone <keenan.huntstone@colorado.edu>
2021-03-17 10:03:01 -0600

    Cleaning up for sharing

--- khuntstone/betatron/synchrotron.py
+++ khuntstone/betatron/synchrotron.py
@@ -3,11 +3,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.constants import c, e, m_e, mu_0
-from scipy.interpolate import InterpolatedUnivariateSpline as ius
+
+
 from scipy.special import kv
 
 import thomas as th
-
 inv_c = 1 / c
 def analytic_synch(w, gamma, p, angle):
     """
@@ -32,127 +32,115 @@ def analytic_synch(w, gamma, p, angle):
                            + angle**2))
     return t1 * t2 * t3
 
-# Functions for interpolating 
-def get_ders(tau, var):
-    s = ius(tau, var)
-    xp = s.derivative(n = 1)
-    xpp = s.derivative(n = 2)
-    return xp(tau), xpp(tau)
-
-def quad_interp(var, var1n, var2n, tau, tau_int):
-    var_int = np.zeros(len(tau_int))
-    for i in range(len(tau_int)):
-        ind = np.argmin(abs(tau_int[i] - tau))
-        taun = tau[ind]
-        var_int[i] = var[ind]\
-                     + var1n[ind] * (tau_int[i] - taun) \
-                     + var2n[ind] * (tau_int[i] - taun)**2
-    return var_int
-
-def lin_interp(var, var1n, tau, tau_int):
-    var_int = np.zeros(len(tau_int))
-    for i in range(len(tau_int)):
-        ind  = np.argmin(abs(tau_int[i] - tau))
-        taun = tau[ind]
-        var_int[i] = var[ind] + var1n[ind] * (tau_int[i] - taun)
-    return var_int
-
-# Thomas Algorithm implemented below
-    
 # Initial parameters from paper
+Nw = int(1e3)
 w0    = 2.5e15
-w     = np.linspace(1e0, 1e7, 10000) * w0
-B     = m_e * w0 / e
-dtau  = np.pi*1e-4 / w0
-gamma = 1000
-beta  = np.sqrt(1 - 1/gamma**2)
-p     = gamma * m_e * c / (e * B)
-angle    = 0
-beta = np.sqrt(1 - (1/gamma**2))
-dt   = gamma * dtau
-N    = 1000
-t    = np.linspace(-N * dt, N * dt, N*2)
-tau  = t / gamma
+w     = np.linspace(w0, w0*1e7, Nw)
 
+gamma = 1000
+gamma2 = gamma*gamma
+inv_gamma2 = 1 / gamma2
+
+angle    = 0
+
+beta  = np.sqrt(1 - 1/gamma2)
+B     = m_e * w0 / e
+p     = gamma * m_e * c / (e * B)
+# Time step
+dtau  = np.pi*1e-4 / w0
+dt   = gamma * dtau
+
+# Time for 1 rotation
+t_rot = (2 * np.pi * p) / (0.999 * c)
+# Useful variables
+inv_pi = 1 / np.pi
+inv_p = 1/p
+
+t    = np.arange(0, 0.1*t_rot, dt)
+Nt   = 10000
+t    = np.linspace(-0.5*Nt*dt, 0.5*Nt*dt, Nt)
+tau  = t / gamma
 # Particle position
-x    = p * np.sin(beta * c * t / p)
-y    = p * (1 - np.cos(beta * c * t / p))
+x    = p * np.sin(beta * c * t * inv_p)
+y    = p * (1 - np.cos(beta * c * t * inv_p))
 z    = np.zeros(len(x))
-x4   = (c*t, x, y, z)
+x4   = np.array([c*t, x, y, z], dtype = "longdouble")
 
 # Particle velocity
-v40 = np.zeros(len(tau)) + c * gamma
-v41 = get_ders(tau, x)[0]
-v42 = get_ders(tau, y)[0]
-v43 = get_ders(tau, z)[0]
-v4  = (v40, v41, v42, v43)
+v00 = th.get_dtau(tau, x)[0]
+v01 = th.get_dtau(tau, y)[0]
+v02 = th.get_dtau(tau, z)[0]
+v0  = inv_c*np.array([v00, v01, v02], dtype = "longdouble")
 
 # Interpolation of position 
-tau_int = np.linspace(tau[0], tau[-1], 10000)
 # ct, have to do manually
-ct1n   = np.zeros(len(tau)) + gamma * c
-ct2n   = np.zeros(len(tau))
-ct_int = quad_interp(c*t, ct1n, ct2n, tau, tau_int)
+ct1   = np.zeros(len(tau)) + gamma * c
+ct2   = np.zeros(len(tau))
 # x
-x1n, x2n = get_ders(tau, x4[1])
-x2n      = 0.5 * x2n
-x_int    = quad_interp(x, x1n, x2n, tau, tau_int)
+x1, x2 = th.get_dtau(tau, x4[1])
+x2      = 0.5 * x2
 # y 
-y1n, y2n = get_ders(tau, x4[2])
-y2n      = 0.5 * y2n
-y_int    = quad_interp(y, y1n, y2n, tau, tau_int)
+y1, y2 = th.get_dtau(tau, x4[2])
+y2      = 0.5 * y2
 # z
-z1n, z2n = get_ders(tau, x4[3])
-z2n      = 0.5 * z2n
-z_int    = quad_interp(z, z1n, z2n, tau, tau_int)
+z1, z2 = th.get_dtau(tau, x4[3])
+z2      = 0.5 * z2
 
-x41n   = (ct1n, x1n, y1n, z1n)
-x42n   = (ct2n, x2n, y2n, z2n)
-x4_int = (ct_int, x_int, y_int, z_int)
+
+x41   = np.array([ct1, x1, y1, z1], dtype = "longdouble")
+x42   = np.array([ct2, x2, y2, z2], dtype = "longdouble")
 
 # Interpolation of velocity 
 # v_ct
-vct1n   = np.zeros(len(tau))
-vct_int = lin_interp(v40, vct1n, tau, tau_int)
+vct1 = np.zeros(len(tau))
 # vx
-vx1n = get_ders(tau, v4[1])[0]
-vx_int = lin_interp(v41, vx1n, tau, tau_int)
+vx1  = th.get_dtau(tau, v0[0])[0]
 # vy
-vy1n = get_ders(tau, v4[2])[0]
-vy_int = lin_interp(v42, vy1n, tau, tau_int)
+vy1  = th.get_dtau(tau, v0[1])[0]
 # vz
-vz1n = get_ders(tau, v4[3])[0]
-vz_int = lin_interp(v43, vz1n, tau, tau_int)
-
-v4_int = (vct_int, vx_int, vy_int, vz_int)
-v1n   = (vx1n, vy1n, vz1n)
+vz1  = th.get_dtau(tau, v0[2])[0]
+v1   = inv_c * np.array([vx1, vy1, vz1], dtype = "longdouble")
 
 # Computation of on-axis radiation
 # Set observation vector and angle
-s_hat = [1, 0, 0] # on-axis
+s_hat = np.array([1, 0, 0], dtype = "longdouble") # on-axis
 theta = 90 * np.pi / 180
-# Compute radiation (computationally and analytically)
-d2I_comp = th.get_d2I(w, s_hat, x4, x41n, x42n, v4, v1n, dtau, theta)
-d2I_ana  = analytic_synch(w, gamma, p, angle)
-# Plot & Compare
-fig1 = plt.figure()
-ax1  = fig1.add_subplot(121)
-ax2  = fig1.add_subplot(122)
-ax1.set_xlabel(r'$\omega / \omega_0$')
-ax1.set_ylabel(r'$\frac{d^2I}{d\omega d\Omega}$')
-ax1.set_title("Thomas Algorithm")
-ax2.set_xlabel(r'$\omega / \omega_0$')
-ax2.set_ylabel(r'$\frac{d^2I}{d\omega d\Omega}$')
-ax2.set_title("Analytical Solution")
-ax1.semilogx(w / w0, d2I_comp)
-ax2.semilogx(w / w0, d2I_ana)
+
+# Parameter for switching between Frensel and Taylor
+small     = 1e-3
+# Compute radiation
+d2I_comp  = th.get_d2I(w, s_hat, gamma, x4, x41, x42, v0, v1, dtau,\
+                       theta, small)
+    
+# Plot and compare
+w_ana    = np.linspace(w0, w0*1e7, int(1e6))
+d2I_ana  = analytic_synch(w_ana, gamma, p, angle)
+fig = plt.figure(figsize = (8, 4))
+ax1 = fig.add_subplot(121)
+ax1.set_ylabel(r'$\frac{d^2I}{d\omega d\Omega}$ $\left[J \cdot rad^{-1}' \
+               + r'$\cdot s \cdot sr^{-1}\right]$')
+ax1.set_xlabel(r'$\omega/\omega_0$')
+ax1.set_title('Simulated')
+ax1.semilogx(w/w0, np.real(d2I_comp), '-')
+ax2 = fig.add_subplot(122)
+ax2.semilogx(w_ana/w0, np.real(d2I_ana))
+ax2.set_xlabel(r'$\omega/\omega_0$')
+ax2.set_ylabel(r'$\frac{d^2I}{d\omega d\Omega}$ $\left[J \cdot rad^{-1} '\
+               + r'$\cdot s \cdot sr^{-1}\right]$')
+ax2.set_title("Analytic")
+ax1.set_xlim([1e0, 1e7])
+ax2.set_xlim([1e0, 1e7])
+plt.show()
+fig2 = plt.figure(figsize = (8, 4), dpi=200)
+ax3  = fig2.gca()
+ax3.semilogx(w/w0, np.real(d2I_comp)/max(np.real(d2I_comp)), \
+                           label = "Computed")
+ax3.semilogx(w_ana/w0, np.real(d2I_ana)/max(np.real(d2I_ana)), '--', \
+                               label = "Analytic")
+ax3.legend()
+ax3.set_xlabel(r'$\omega /\omega_0$')
+ax3.set_ylabel(r'$\frac{d^2I}{d\omega d\Omega}$ $\left[J \cdot rad^{-1} '\
+               +'$\cdot s \cdot sr^{-1}\right]$')
+ax3.set_title("Normalized Comparison")
 plt.show()
 
-fig2 = plt.figure()
-ax3  = fig2.gca()
-ax3.set_xlabel(r'$\omega / \omega_0$')
-ax3.set_ylabel(r'$\frac{d^2I}{d\omega d\Omega}$')
-ax3.semilogx(w / w0, d2I_comp / max(d2I_comp), label = "Computational")
-ax3.semilogx(w / w0, d2I_ana / max(d2I_ana), label = "Analytic")
-ax3.legend()
-plt.show()
--- khuntstone/betatron/thomas.py
+++ khuntstone/betatron/thomas.py
@@ -7,57 +7,20 @@ Created on Tue Nov 24 11:43:25 2020
 
 """
 Code to reproduce math/results from Alec Thomas' PR A&B 2010 paper. 
-
-Definition of variables:
-------------------------
-
-chi0n     : array_like,
-            Dot product of 0th order position interpolation coefficient (the 
-            particle trajectory) and 4-wave vector     
-chi1n     : array_like, 
-            Dot product of 1st order position interpolation coefficient and 
-            4-wave vector (Thomas Eq. 18). Array is along time, not particle 
-            number.      
-chi2n     : array_like
-            Dot product of 2nd order position interpolation coefficient and
-            4-wave vector (THomas Eq. 19). Array is along time, not particle 
-            number.     
-dtau      : float
-            Non-interpolated proper time step.   
-ImIn      : array_like
-            imaginary part of spectral intensity integral given by Thomas 
-            Eq. 13 or Eq. 22
-ISx/y/z   : array_like
-            Combination of integrals defined by Thomas Eq. 28
-phi_p/m   : array_like
-            Reduced variables phi_+/- from Thomas Eq. 17
-psi_p/m   : array_like
-            Reduced variables psi_+/- from Thomas Eqs. 15 and 16    
-ReIn      : array_like
-            real part of spectral intensity integral given by Thomas Eq. 12 or
-            Eq. 21
-RSx/y/z   : Array_like
-            Combination of integrals defined by Thomax Eq. 27
-s_hat     : array_like
-            Unit vector of observation
-sign      : int
-            Sign of current frequency value
-theta     : float
-            Observation angle with respect to the z-axis in the y-z plane       
-theta_p/m : array_like
-            Reduced variables theta_+/- from Thomas Eq. 14    
-v4        : array_like
-            Particle 4 velocity array along time.         
-v1n       : array_like
-            1st order velocity interpolation coefficient array along time. 
 """
-
+# Imports and constants
 import numpy as np
 from scipy.constants import c, e, mu_0
+from scipy.interpolate import InterpolatedUnivariateSpline as ius
 from scipy.special import erf
-import matplotlib.pyplot as plt
 
+# Other constants
+inv_c        = 1/c
+inv_pi       = 1/np.pi
+sqrt_2pi     = np.sqrt(2*np.pi)
+inv_sqrt_2pi = 1 / sqrt_2pi
 
+# Math functions
 def get_fresnel(z):
     """
     Function to compute fresnel integrals. Needed as scipy does not handle 
@@ -65,7 +28,7 @@ def get_fresnel(z):
     
     Parameters:
     -----------
-    z : float, or array_like
+    z : complex float or array *scipy.special.erf does not support complex256*
         Input into the fresnel integrals
         
     Returns:
@@ -86,247 +49,264 @@ def get_fresnel(z):
     
     return S, C
 
+def sincu(x):
+    """
+    Unnormalized sinc function
+    """
+    return np.sin(x)/x
 
-def get_phis(chi1n, chi2n, dtau):
+# Interpolation coeff function
+def get_dtau(tau, var):
     """
-    Function to compute reduced variables phi pluse/minus
+    Returns 1st and 2nd order derivatives w.r.t. proper time
     """
-    phi_p = (0.25 * dtau**2) * chi2n + (0.5 * dtau) * chi1n
-    phi_m = (0.25 * dtau**2) * chi2n - (0.5 * dtau) * chi1n
+    s = ius(tau, var)
+    xp = s.derivative(n=1)
+    xpp = s.derivative(n=2)
+    return xp(tau), xpp(tau)
+
+"""
+Below are functions for computing many of the equations/variables in Thomas'
+paper. Note they are not called for get_d2I as that nearly doubles computation
+time. They are however useful for debugging. 
+"""
+
+def get_chis(w, s_hat, x4, x41, x42):
+    """
+    Function to compute reduced chi variables
+    """
+    kappa = w * inv_c * np.array([1, s_hat[0], s_hat[1], s_hat[2]])
+    chi0  = -kappa[0]*x4[0]+kappa[1]*x4[1]+kappa[2]*x4[2]+kappa[3]*x4[3]
+    chi1  = -kappa[0]*x41[0]+kappa[1]*x41[1]+kappa[2]*x41[2]+kappa[3]*x42[3]
+    chi2  = -kappa[0]*x42[0]+kappa[1]*x42[1]+kappa[2]*x42[2]+kappa[3]*x42[3]
+    inv_chi1         = 1/chi1
+    inv_chi2         = 1/chi2
+    sqrt_chi2        = np.complex128(np.sqrt(abs(chi2)))
+    z_ind            = np.argwhere(chi2<0)
+    sqrt_chi2[z_ind] = 1j*sqrt_chi2[z_ind]
+    inv_sqrt_chi2    = 1/sqrt_chi2
+    return chi0, chi1, chi2, inv_chi1, inv_chi2, inv_sqrt_chi2
+
+def get_phis(chi1, chi2, dtau):
+    """
+    Function to compute reduced variables phi plus/minus
+    """
+    phi_p = (0.25 * dtau*dtau) * chi2 + (0.5 * dtau) * chi1
+    phi_m = (0.25 * dtau*dtau) * chi2 - (0.5 * dtau) * chi1
     
     return phi_p, phi_m
 
-def get_psis(chi1n, chi2n, v0n, v1n):
+def get_psis(chi1, chi2, inv_chi2, inv_sqrt_chi2, v0, v1):
     """
     Function to compute reduced variables Psi plus/minus
     """
-    psi_p = np.zeros(len(chi2n), dtype = "complex128")
-    psi_m = np.zeros(len(chi2n), dtype = "complex128")
-    psi_base = np.zeros(len(chi2n), dtype = "complex128")
-    psi_base += np.sqrt(2 * np.pi / abs(chi2n)) \
-               * (2 * chi2n * v0n - chi1n * v1n)
-    psi_base[chi2n < 0] = 1j * psi_base[chi2n <0]
-    psi_p += psi_base * np.cos(0.25 * chi1n**2 / chi2n)
-    psi_m +=  psi_base * np.sin(0.25 * chi1n**2 / chi2n)
+    psi_base = sqrt_2pi*inv_sqrt_chi2*(2*chi2*v0-chi1*v1)
+    psi_p    = psi_base*np.cos(0.25*chi1*chi1*inv_chi2)
+    psi_m    = psi_base*np.sin(0.25*chi1*chi1*inv_chi2)
     
     return psi_p, psi_m
 
-def get_thetas(chi1n, chi2n, dtau):
+def get_thetas(chi1, chi2, inv_sqrt_chi2, dtau):
     """
     Function to compute the reduced variables theta plus/minus 
     """
+    theta_p = inv_sqrt_2pi*inv_sqrt_chi2*(chi1+chi2*dtau)
+    theta_m  = inv_sqrt_2pi*inv_sqrt_chi2*(chi1-chi2*dtau)
     
-    theta_p = np.zeros(len(chi2n), dtype = "complex128")
-    theta_m = np.zeros(len(chi2n), dtype = "complex128")
-    theta_p += (chi1n + chi2n * dtau) / np.sqrt(2 * np.pi * abs(chi2n))
-    theta_m += (chi1n - chi2n * dtau) / np.sqrt(2 * np.pi * abs(chi2n))
-    
-    theta_p[chi2n < 0] = 1j * theta_p[chi2n < 0]
-    theta_m[chi2n < 0] = 1j * theta_m[chi2n < 0]
     return theta_p, theta_m
+    
 
-def fresnel_I(chi1n, chi2n, v0n, v1n, dtau, sign, plot = False):
+def fresnel_I(chi1, chi2, inv_chi2, inv_sqrt_chi2, v0, v1, dtau, sign):
     
     """
     Function to compute the spectral intensity integral (Eqs. 12 & 13) using
-    Fresnel integrals. NOTE: This computes one vector component for one 
-    particle. Must call for each vector component with a different velocity 
-    input and loop over all particles. 
+    Fresnel integrals.
     """
     
-    # Preallocate for complex values
-    ReIn = np.zeros(len(chi2n), dtype = "complex128")
-    ImIn = np.zeros(len(chi2n), dtype = "complex128")
-    
     # Compute reduced variables
-    phi_p, phi_m = get_phis(chi1n, chi2n, dtau)
+    phi_p, phi_m = get_phis(chi1, chi2, dtau)
     
-    psi_p, psi_m = get_psis(chi1n, chi2n, v0n, v1n)
+    psi_p, psi_m = get_psis(chi1, chi2, inv_chi2, inv_sqrt_chi2, v0, v1)
     
-    theta_p, theta_m = get_thetas(chi1n, chi2n, dtau)
+    theta_p, theta_m = get_thetas(chi1, chi2, inv_sqrt_chi2, dtau)
     
     
-    Sp, Cp = get_fresnel(theta_p)
-    Sm, Cm  = get_fresnel(theta_m)
+    Sp, Cp = get_fresnel(np.complex128(theta_p))
+    Sm, Cm  = get_fresnel(np.complex128(theta_m))
     
     sin_p = np.sin(phi_p)
     sin_m = np.sin(phi_m)
     cos_p = np.cos(phi_p)
     cos_m = np.cos(phi_m)
     
-    ReIn += psi_p * (Cp - Cm) + psi_m * (Sp - Sm) + 2 * v1n * (sin_p - sin_m)
-    ReIn = (0.25 / chi2n) * ReIn
+    ReIn = (0.25*inv_chi2) * (psi_p*(Cp-Cm) \
+                              + psi_m*(Sp-Sm) \
+                              +2*v1*(sin_p-sin_m))
     
-    ImIn += psi_p * (Sp - Sm) - psi_m * (Cp - Cm) - 2 * v1n * (cos_p - cos_m)
-    ImIn = sign * (0.25 / chi2n) * ImIn
+    ImIn = (0.25*inv_chi2)*(psi_p*(Sp-Sm)\
+                            -psi_m*(Cp-Cm)\
+                            -2*v1*cos_p-cos_m)
     
-    if plot:
-        # Phis
-        fig = plt.figure(figsize = (8, 6))
-        ax1  = fig.add_subplot(121)
-        ax2  = fig.add_subplot(122)
-        ax1.plot(phi_p, label = "phi +")
-        ax1.legend()
-        ax2.plot(phi_m, label = "phi -")
-        ax2.legend()
-        plt.show()
-        # psis (real)
-        fig = plt.figure(figsize = (8, 6))
-        ax1  = fig.add_subplot(121)
-        ax2  = fig.add_subplot(122)
-        ax1.plot(np.real(psi_p), label = "Re psi +")
-        ax1.legend()
-        ax2.plot(np.real(psi_m), label = "Re psi -")
-        ax2.legend()
-        plt.show()
-        # psi
-        fig = plt.figure(figsize = (8, 6))
-        ax1  = fig.add_subplot(121)
-        ax2  = fig.add_subplot(122)
-        ax1.plot(np.imag(psi_p), label = "Im psi +")
-        ax1.legend()
-        ax2.plot(np.imag(psi_m), label = "Im psi -")
-        ax2.legend()
-        plt.show()
-        # Thetas (real)
-        fig = plt.figure(figsize = (8, 6))
-        ax1  = fig.add_subplot(121)
-        ax2  = fig.add_subplot(122)
-        ax1.plot(np.real(theta_p), label = "Re Theta +")
-        ax1.legend()
-        ax2.plot(np.real(theta_m), label = "Re Theta -")
-        ax2.legend()
-        plt.show()
-        # Thetas (imag)
-        fig = plt.figure(figsize = (8, 6))
-        ax1  = fig.add_subplot(121)
-        ax2  = fig.add_subplot(122)
-        ax1.plot(np.imag(theta_p), label = "Im Theta +")
-        ax1.legend()
-        ax2.plot(np.imag(theta_m), label = "Im Theta -")
-        ax2.legend()
-        plt.show()
-        
-        #Re and Im
-        # psi
-        fig = plt.figure(figsize = (8, 6))
-        ax1  = fig.add_subplot(121)
-        ax2  = fig.add_subplot(122)
-        ax1.plot(ReIn, label = "Real I")
-        ax1.legend()
-        ax2.plot(ImIn, label = "Imag I")
-        ax2.legend()
-        plt.show()
+    
         
     return ReIn, ImIn
 
-def taylor_I(chi1n, chi2n, v0n, v1n, dtau, sign):
+def taylor_I(chi1, chi2, inv_chi1, v0, v1, dtau, sign):
     """
     Function to compute the spectral intensity integral (Eqs. 21 & 22), using
-    a Taylor expansion method. NOTE: This computes one vector component for 
-    one particle. Must call for each vector component with a different 
-    velocity input and loop over all particles. 
+    a Taylor expansion method.
     """
+
+    sinc_chi1dtau = sincu(0.5*chi1*dtau)
+    cos_chi1dtau = sincu(0.5*chi1*dtau)
+    I0  = sinc_chi1dtau*dtau
+    I1  = (dtau*inv_chi1)*(sinc_chi1dtau - cos_chi1dtau)
+    I2  = (0.25*dtau*dtau*dtau) * sinc_chi1dtau - 2*inv_chi1*I1
     
-    def sinc(x):
-        return np.sin(x) / x
-    
-    # Computed reduced variables
-    I0n = sinc(0.5 * chi1n * dtau) * dtau
-    I1n = (dtau / chi1n) \
-           * (sinc(0.5 * chi1n * dtau) - np.cos(0.5 * chi1n * dtau))
-    I2n = (0.25 * dtau**3) * sinc(0.5 * chi1n * dtau) - (2 / chi1n) * I1n
-    
-    ReIn = v0n * I0n
-    ImIn = v1n * I1n + sign * (chi2n * v0n * I2n)
+    ReIn = v0*I0
+    ImIn = v1*I1+sign*(chi2*v0*I2)
     
     return ReIn, ImIn
 
-def get_chis(w, s_hat, x4, x41n, x42n):
-    """
-    Function to compute chi0n, chi1n, and chi2n
-    """
-    kappa = (w/c) * np.array([1, s_hat[0], s_hat[1], s_hat[2]])
-    chi0n = -kappa[0] * x4[0] + kappa[1] * x4[1] + kappa[2] * x4[2]\
-            + kappa[3] * x4[3]
-    chi1n = -kappa[0] * x41n[0] + kappa[1] * x41n[1] + kappa[2] * x41n[2] \
-            + kappa[3] * x41n[3]
-    chi2n = -kappa[0] * x42n[0] + kappa[1] * x42n[1] + kappa[2] * x42n[2] \
-            + kappa[3] * x42n[3]
-    chi0n = np.longdouble(chi0n)
-    chi1n = np.longdouble(chi1n)
-    chi2n = np.longdouble(chi2n)
+def get_d2I(w, s_hat, gamma, x4, x41, x42, v0, v1, dtau, theta, small=1e-3):
     
-    return chi0n, chi1n, chi2n
-
-def get_d2I(w, s_hat, x4, x41n, x42n, v4, v1n, dtau, theta):
-    """
-    Function to compute the spectral intensity from Thomas eq. (27)
-    """
-    
-    # Looping for now, speedup later
-    d2I = np.zeros(len(w))
-
-    
+    # Preallocate
+    d2I = np.zeros(len(w), dtype = "complex128")
+    #dchi2 = np.zeros((len(w), len(x4[0])))
+    # Verbosity
+    status = len(w) / 10
+            
     for i in range(len(w)):
+        if (i+1)%status==0:
+            print(i+1, "of", len(w))
         om = w[i]
-        sign = np.sign(om)
-        chi0n, chi1n, chi2n = get_chis(om, s_hat, x4, x41n, x42n)
+        kappa = om * inv_c * np.array([1, s_hat[0], s_hat[1], s_hat[2]])
         
-        # Preallocate
-        RIx = np.zeros(len(chi2n), dtype = "complex128")
-        RIy = np.zeros(len(chi2n), dtype = "complex128")
-        RIz = np.zeros(len(chi2n), dtype = "complex128")
+        chi0  = -kappa[0]*x4[0] + kappa[1]*x4[1] + kappa[2]*x4[2]\
+                + kappa[3]*x4[3]
+                
+        chi1 = -kappa[0]*x41[0] + kappa[1]*x41[1] + kappa[2]*x41[2] \
+                + kappa[3]*x41[3]
+                
+        chi2 = -kappa[0]*x42[0] + kappa[1]*x42[1] + kappa[2]*x42[2] \
+              + kappa[3]*x42[3]
         
-        IIx = np.zeros(len(chi2n), dtype = "complex128")
-        IIy = np.zeros(len(chi2n), dtype = "complex128")
-        IIz = np.zeros(len(chi2n), dtype = "complex128")
         
-        # Index where to use Fresnel solution or Taylor approximate
-        f_ind = np.squeeze(np.argwhere(abs(chi2n * dtau**2) > 1e-3))
-        t_ind = np.squeeze(np.argwhere(abs(chi2n * dtau**2) <= 1e-3))
+        chi0 = np.longdouble(chi0)
+        chi1 = np.longdouble(chi1)
+        chi2 = np.longdouble(chi2)
         
-        # Compute Fresnel solution
-        fresRIx, fresIIx = fresnel_I(chi1n[f_ind], chi2n[f_ind], \
-                                     v4[1][f_ind], v1n[0][f_ind], dtau, sign)
-        fresRIy, fresIIy = fresnel_I(chi1n[f_ind], chi2n[f_ind], \
-                                     v4[2][f_ind], v1n[1][f_ind], dtau, sign)
-        fresRIz, fresIIz = fresnel_I(chi1n[f_ind], chi2n[f_ind], \
-                                     v4[3][f_ind], v1n[2][f_ind], dtau, sign)
         
-        RIx[f_ind] = fresRIx
-        RIy[f_ind] = fresRIy
-        RIz[f_ind] = fresRIz
-        IIx[f_ind] = fresIIx
-        IIy[f_ind] = fresIIy
-        IIz[f_ind] = fresIIz
-        # Compute Taylor approximation
-        tayRIx, tayIIx = taylor_I(chi1n[t_ind], chi2n[t_ind], v4[1][t_ind],
-                                     v1n[0][t_ind], dtau, sign)
-        tayRIy, tayIIy = taylor_I(chi1n[t_ind], chi2n[t_ind], v4[2][t_ind],
-                                     v1n[1][t_ind], dtau, sign)
-        tayRIz, tayIIz = taylor_I(chi1n[t_ind], chi2n[t_ind], v4[3][t_ind],
-                                     v1n[2][t_ind], dtau, sign)
         
-        RIx[t_ind] = tayRIx
-        RIy[t_ind] = tayRIy
-        RIz[t_ind] = tayRIz
-        IIx[t_ind] = tayIIx
-        IIy[t_ind] = tayIIy
-        IIz[t_ind] = tayIIz
+        #dchi2[i, :] = chi2
+        # Only divide once
+        inv_chi1         = 1/chi1
+        inv_chi2         = 1/chi2
+        z_ind            = np.argwhere(chi2 < 0)
+        sqrt_chi2        = np.complex128(np.sqrt(abs(chi2)))
+        sqrt_chi2[z_ind] = 1j*sqrt_chi2[z_ind]
+        inv_sqrt_chi2    = 1/sqrt_chi2
         
-        # Compute d2I (Thomas Equations 27-29)
-        RSx = np.sum(RIx * np.cos(chi0n) - IIx * np.sin(chi0n))
-        RSy = np.sum(RIy * np.cos(chi0n) - IIy * np.sin(chi0n))
-        RSz = np.sum(RIz * np.cos(chi0n) - IIz * np.sin(chi0n))
+        # Fresnel approach
+        phi_p = (0.25*dtau*dtau)*chi2 + (0.5*dtau*chi1)
+        phi_m = (0.25*dtau*dtau)*chi2 + (0.5*dtau*chi1)
         
-        ISx = np.sum(IIx * np.cos(chi0n) + RIx * np.sin(chi0n))
-        ISy = np.sum(IIy * np.cos(chi0n) + RIy * np.sin(chi0n))
-        ISz = np.sum(IIz * np.cos(chi0n) + RIz * np.sin(chi0n))
+        theta_p = inv_sqrt_2pi*inv_sqrt_chi2*(chi1+chi2*dtau)
+        theta_m = inv_sqrt_2pi*inv_sqrt_chi2*(chi1-chi2*dtau)
+        
+        
+        psi_base_x = sqrt_2pi*inv_sqrt_chi2*(2*chi2*v0[0]-chi1*v1[0])
+        psi_base_y = sqrt_2pi*inv_sqrt_chi2*(2*chi2*v0[1]-chi1*v1[1])
+        psi_base_z = sqrt_2pi*inv_sqrt_chi2*(2*chi2*v0[2]-chi1*v1[2])
+    
+        psi_px     = psi_base_x*np.cos(0.25*chi1*chi1*inv_chi2)
+        psi_py     = psi_base_y*np.cos(0.25*chi1*chi1*inv_chi2)
+        psi_pz     = psi_base_z*np.cos(0.25*chi1*chi1*inv_chi2)
+    
+        psi_mx     = psi_base_x*np.sin(0.25*chi1*chi1*inv_chi2)
+        psi_my     = psi_base_y*np.sin(0.25*chi1*chi1*inv_chi2)
+        psi_mz     = psi_base_z*np.sin(0.25*chi1*chi1*inv_chi2)
+        
+        Sp, Cp = get_fresnel(np.complex128(theta_p))
+        Sm, Cm = get_fresnel(np.complex128(theta_m))
+        
+        sin_p  = np.sin(phi_p)
+        sin_m  = np.sin(phi_m)
+        cos_p = np.cos(phi_p)
+        cos_m = np.cos(phi_m)
+        
+        RIfx  = (0.25*inv_chi2) * (psi_px*(Cp-Cm) \
+                                  + psi_mx*(Sp-Sm) \
+                                  +2*v1[0]*(sin_p-sin_m))
+        RIfy  = (0.25*inv_chi2) * (psi_py*(Cp-Cm) \
+                                  + psi_my*(Sp-Sm) \
+                                  +2*v1[1]*(sin_p-sin_m))
+        RIfz  = (0.25*inv_chi2) * (psi_pz*(Cp-Cm) \
+                                  + psi_mz*(Sp-Sm) \
+                                  +2*v1[2]*(sin_p-sin_m))
+        
+        IIfx  = (0.25*inv_chi2)*(psi_px*(Sp-Sm)\
+                                -psi_mx*(Cp-Cm)\
+                                -2*v1[0]*cos_p-cos_m)
+        
+        IIfy  = (0.25*inv_chi2)*(psi_py*(Sp-Sm)\
+                                -psi_my*(Cp-Cm)\
+                                -2*v1[1]*cos_p-cos_m)
+        
+        IIfz  = (0.25*inv_chi2)*(psi_pz*(Sp-Sm)\
+                                -psi_mz*(Cp-Cm)\
+                                -2*v1[2]*cos_p-cos_m)
+        
+        # Taylor approximation
+        
+        sinc_chi1dtau = sincu(0.5*chi1*dtau)
+        cos_chi1dtau = sincu(0.5*chi1*dtau)
+        I0  = sinc_chi1dtau*dtau
+        I1  = (dtau*inv_chi1)*(sinc_chi1dtau - cos_chi1dtau)
+        I2  = (0.25*dtau*dtau*dtau) * sinc_chi1dtau - 2*inv_chi1*I1
+        
+        
+        RItx = v0[0]*I0
+        RIty = v0[1]*I0
+        RItz = v0[2]*I0
+        
+        IItx = v1[0] * I1 + (chi2 * v0[0] * I2)
+        IIty = v1[1] * I1 + (chi2 * v0[1] * I2)
+        IItz = v1[2] * I1 + (chi2 * v0[2] * I2)
+        
+        # Apply taylor expansion filter
+        comp_par = abs(chi2*dtau*dtau)
+        t_ind    = np.argwhere(comp_par <= small)
+        
+        RIx        = RIfx
+        RIx[t_ind] = RItx[t_ind]
+        RIy        = RIfy
+        RIy[t_ind] = RIty[t_ind]
+        RIz        = RIfz
+        RIz[t_ind] = RItz[t_ind]
+        
+        IIx        = IIfx
+        IIx[t_ind] = IItx[t_ind]
+        IIy        = IIfy
+        IIy[t_ind] = IIty[t_ind]
+        IIz        = IIfz
+        IIz[t_ind] = IItz[t_ind]
+        
+        # Cartesian form 
+        RSx = np.sum(RIx * np.cos(chi0) - IIx * np.sin(chi0))
+        RSy = np.sum(RIy * np.cos(chi0) - IIy * np.sin(chi0))
+        RSz = np.sum(RIz * np.cos(chi0) - IIz * np.sin(chi0))
+    
+        ISx = np.sum(IIx * np.cos(chi0) + RIx * np.sin(chi0))
+        ISy = np.sum(IIy * np.cos(chi0) + RIy * np.sin(chi0))
+        ISz = np.sum(IIz * np.cos(chi0) + RIz * np.sin(chi0))  
         
         d2I[i] = RSx**2 + ISx**2 \
-             + (RSy * np.cos(theta) - RSz * np.sin(theta))**2 \
-             + (ISy * np.cos(theta) - ISz * np.sin(theta))**2
-    d2I = d2I * (mu_0 * e**2 * (w)**2) / (16 * np.pi**3)  
-        
+                 + (RSy * np.cos(theta) - RSz * np.sin(theta))**2 \
+                 + (ISy * np.cos(theta) - ISz * np.sin(theta))**2
+    
+  
+    d2I = d2I * (mu_0*e*e*c*w*w*gamma*gamma)*(inv_pi*inv_pi*inv_pi)/(16)    
+    
+    
     return d2I
