Corrected the aberration calculation in optic Added some getter functions in laserbeam

python/beam/beams/laserbeam.py
@@ -144,6 +144,12 @@ class Laser(beam.Beam):
         fx = fftfreq(self.Nx, dx)
         fy = fftfreq(self.Ny, dy)
         return fx, fy
+    
+    def get_If(self):
+        """ Get the intensity in frequency domain."""
+        beam = self
+        If = abs(fftshift(beam.fft(beam.e)))**2
+        return If
         
     # File managment
     #--------------------------------------------------------------------------
@@ -251,6 +257,7 @@ class Laser(beam.Beam):
             Nk= self.Ny
             K= self.Y
         e1 = np.zeros((Nz, Nk), dtype='complex128')
+        #TODO This now only works for 2D beam, set it so that it also works for cyl beam
         for i in range(int(Nz0), Nz):
             if axis== 0:
                 e1[i, :] = self.load_field(i+1)[0][:, int(self.Ny/2)]
@@ -374,7 +381,7 @@ class Laser(beam.Beam):
         plt.subplot(133)
         plt.plot(fx, If[:, indy], label='x')
         plt.plot(fy, If[indx, :], '--', label='y')
-        plt.legend()
+        plt.legend()    
         plt.xlabel(r'$f_x$ (um$^{-1}$)')
         plt.ylabel(r'Intensity (arb unit)')
         if flim != None:

python/beam/elements/optic.py
@@ -309,7 +309,8 @@ class Mirror(Phase):
         super().__init__(params)
     
     def initialize_phase(self):
-        fx= self.f* (1/np.cos(np.deg2rad(self.theta_i))-1)+ self.f
+        fx= self.f- self.f*(np.sin(np.deg2rad(self.theta_i))*np.tan(np.deg2rad(self.theta_i)))
+#        fx= self.f- self.f* (1/np.cos(np.deg2rad(self.theta_i))-1)
         fy= self.f
         phi = -self.k * (self.x[:, None]**2/ (2*fx)+self.y[None, :]**2/ (2*fy))
         super().initialize_phase(phi)