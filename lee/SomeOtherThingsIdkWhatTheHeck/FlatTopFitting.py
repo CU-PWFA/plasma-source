f3ee71d0fa3d8b6f3d5d85a30d8d6d34e24f2530
Valentina-Lee <54038737+Valentina-Lee@users.noreply.github.com>
2025-01-29 16:02:12 -0700

    Delete lee directory

--- lee/SomeOtherThingsIdkWhatTheHeck/FlatTopFitting.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb  3 15:39:40 2020
-
-@author: valentina_lee
-"""
-
--- lee/SomeOtherThingsIdkWhatTheHeck/ProbeRing092619.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Sep 26 09:34:20 2019
-
-@author: valentina_lee
-"""
-
